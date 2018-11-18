4e43eec9bbaa55b2da960e56a216138b8c3d5c35
Fermat <zhengzangw@gmail.com>
2018-11-18 13:55:39 +0800

    Commit work already done.

---
+++ macro.v
@@ -0,0 +1,5 @@
+//***** COMMON MICRO *****
+`define RSTENABLE 1'b1 //reset signal valid
+`define RSTDISABLE 1'b0 // reset signal invalid
+`define CHIPDISABLE 1'b0 // Chip disable
+`define CHIPENABLE 1'b1 // Chip enabled
---
+++ pc_reg.v
@@ -0,0 +1,27 @@
+`resetall
+`include "macro.v"
+
+module pc_reg(
+	input wire			clk,
+	input wire			rst,
+	output reg[31:0]	pc,
+	output reg			ce
+);
+
+	always @ (posedge clk) begin
+		if (rst == `RSTENABLE) begin
+			ce <= `CHIPDISABLE;
+		end else begin
+			ce <= `CHIPENABLE;
+		end
+	end
+		
+	always @ (posedge clk) begin
+		if (ce == `CHIPDISABLE) begin
+			pc <= 32'h00000000;
+		end else begin
+			pc <= pc + 4'h4;
+		end
+	end
+	
+endmodule 
