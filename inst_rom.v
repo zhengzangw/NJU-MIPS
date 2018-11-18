370f4c53cfd19c088ecf066e8a7a803aecdf866e
Fermat <zhengzangw@gmail.com>
2018-11-18 16:51:18 +0800

    add module inst_rom

--- inst_rom.v
+++ inst_rom.v
@@ -1,2 +1,20 @@
 `include "macro.v"
+module inst_rom(
+    input wire ce,
+    input wire[`INSTADDRBUS] addr,
+    output reg[`INSTBUS] inst
+);
 
+    reg [`INSTBUS] inst_mem[0:`INSTMEMNUM-1];
+
+    initial $readmemh("inst_rom.data", inst_mem);
+
+    always @(*) begin
+        if (ce == `CHIPDISABLE) begin
+            inst <= `ZEROWORD;
+        end else begin
+            inst <= inst_mem[addr[`INSTMEMNUMLOG2+1:2]];
+        end
+    end
+
+endmodule
